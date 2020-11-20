30fcf0ab7b5021d61691bf038e0ca4bf14b46bb8
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-19 18:55:34 -0600

    added basic code for user resolver and authorization

---
+++ lib/graphql/resolvers/User/index.ts
@@ -0,0 +1,44 @@
+import { IResolvers } from "apollo-server-express";
+import { Request } from "express";
+import { Database, User } from "../../../lib/types";
+import { authorize } from "../../../lib/utils";
+import {
+  UserArgs,
+  UserBookingsArgs,
+  UserBookingsData,
+  UserListingsArgs,
+  UserListingsData,
+} from "./types";
+
+export const userResolvers: IResolvers = {
+  Query: {
+    user: async (
+      _root: undefined,
+      { id }: UserArgs,
+      { db, req }: { db: Database; req: Request }
+    ): Promise<User> => {
+      try {
+        const user = await db.users.findOne({ _id: id });
+
+        if (!user) {
+          throw new Error("user can't be found");
+        }
+
+        const viewer = await authorize(db, req);
+
+        if (viewer && viewer._id === user._id) {
+          user.authorized = true;
+        }
+
+        return user;
+      } catch (error) {
+        throw new Error(`Failed to query user: ${error}`);
+      }
+    },
+  },
+  User: {
+    id: (user: User): string => {
+      return user._id;
+    },
+  },
+};
