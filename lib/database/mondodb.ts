efacc0f530bcc20195c2927b548def453de18e89
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-20 07:08:52 -0600

    updated mongodb.ts to connect to database

--- lib/database/mondodb.ts
+++ lib/database/mondodb.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, Db } from "mongodb";
 import { Database, User } from "../../lib/types";
 
 const {DB_USER, DB_PASSWORD, DB_CLUSTER } = process.env
@@ -12,34 +12,30 @@ if(!DB_USER) {
   throw new Error('Please define the DB_CLUSTER env variable')
 } else {
 url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
-
 }
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentiatlly
- * during API Route usage.
- */
-
-let cached = global.mongo
-
-if (!cached) cached = global.mongo = {}
-
-
+export let client: MongoClient;
+export let database: Db;
 
 //Promise<> declares the type of the the whole return function as a promise that when resolved be an object of type database
 //Database function is asynchronous.
 export const connectDatabase = async (): Promise<Database> => {
+  
+  if(!database) {
+    console.info(`Connecting to database ${url}`)
+    
   const client = await MongoClient.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
-  const db = client.db("main");
+  database = client.db("main");
+
+  }
 
   return {
     //could have declared type of collection individually
     //adding the user type as import and adding it as type for db.collection function it ensures data is returned as it should be
-    users: db.collection<User>("users"),
+    users: database.collection<User>("users"),
   };
 };
