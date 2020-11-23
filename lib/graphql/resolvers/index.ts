0727b33bd08e6125019b5417ffe62890255a0d72
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-22 18:49:10 -0600

    added viewer resolver to be exported as a resolver

--- lib/graphql/resolvers/index.ts
+++ lib/graphql/resolvers/index.ts
@@ -1,6 +1,8 @@
 import merge from "lodash.merge";
 import { userResolvers } from './User';
+import { viewerResolvers } from './Viewer';
 
 export const resolvers = merge (
     userResolvers,
+    viewerResolvers
 )
